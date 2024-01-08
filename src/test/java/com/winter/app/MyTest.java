2384ba3aec52ec356a49ace0f5c06f3c4a01d98e
sangyeol9 <xhdfud1@naver.com>
2024-01-08 12:36:22 +0900

    Revert "Revert "20240108 bank""
    This reverts commit e0cdf1ce626f32f5a11ae085ae910096bdb7c843.

---
+++ src/test/java/com/winter/app/MyTest.java
@@ -0,0 +1,29 @@
+package com.winter.app;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*-context.xml"})
+public class MyTest {
+
+//	@Autowired
+//	private DataSource dataSource;
+//	
+//	@Test
+//	public void test11() throws Exception {
+//		Connection con= dataSource.getConnection();
+//		assertNotNull(con);
+//	}
+
+}
